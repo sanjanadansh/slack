512490119bba66005e2b8f53133b4123bf985db6
alnutile <alfrednutile@gmail.com>
2016-02-25 15:36:18 -0500

    Payload fix

--- app/AHAPayload.php
+++ app/AHAPayload.php
@@ -12,6 +12,8 @@ namespace App;
 class AHAPayload
 {
 
+    protected $type = 'feature';
+
     protected $payload = [];
 
     protected $domain = 'det.aha.io';
@@ -23,27 +25,32 @@ class AHAPayload
 
     public function getFeatureName()
     {
-        return $this->payload['feature']['name'];
+        return $this->payload[$this->type]['name'];
     }
 
-    public function getFeatureURL()
+    public function getURL()
     {
-        return $this->payload['feature']['url'];
+        return $this->payload[$this->type]['url'];
     }
 
     public function getWorkflowStatus()
     {
-        return $this->payload['feature']['workflow_status']['name'];
+        return $this->payload[$this->type]['workflow_status']['name'];
     }
 
     public function getCreatedBy()
     {
-        return $this->payload['feature']['created_by_user']['name'];
+        return $this->payload[$this->type]['created_by_user']['name'];
+    }
+
+    public function getOwner()
+    {
+        return $this->payload[$this->type]['owner']['name'];
     }
 
     public function getAssignedTo()
     {
-        return $this->payload['feature']['assigned_to_user']['name'];
+        return $this->payload[$this->type]['assigned_to_user']['name'];
     }
 
     public function getPayload()
@@ -59,11 +66,32 @@ class AHAPayload
     public function makeSlackMessage()
     {
 
+        switch($this->getEvent()) {
+            case "create_release":
+                $this->setType('release');
+                $message = sprintf("Example Incoming Release from AHA: Created by %s, URL %s, Owner to %s Status %s",
+                    $this->getCreatedBy(), $this->getUrl(), $this->getAssignedTo(), $this->getWorkflowStatus());
+                break;
+            case "create_feature":
+                $message = sprintf("Example Incoming Feature from AHA: Created by %s, URL %s, Assigned to %s Status %s",
+                    $this->getCreatedBy(), $this->getUrl(), $this->getAssignedTo(), $this->getWorkflowStatus());
+                break;
+            default:
+                $message = "Not sure the type just yet of this payload";
+        }
 
-
-        $message = sprintf("Example Incoming from AHA: Created by %s, Type %s, Assigned to %s",
-            $this->getCreatedBy(), $this->getEvent(), $this->getAssignedTo());
 
         return $message;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
     }
 }
