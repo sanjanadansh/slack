5d5f01f6e2a1c2f12024107a35be94da038be10b
alnutile <alfrednutile@gmail.com>
2016-02-25 15:28:39 -0500

    Payload fix

--- app/Http/Controllers/AHAController.php
+++ app/Http/Controllers/AHAController.php
@@ -43,10 +43,10 @@ class AHAController extends Controller
         {
             $payload = $request->input();
 
-            //File::put('/tmp/payload.json', json_encode($payload, JSON_PRETTY_PRINT));
+            File::put('/tmp/payload_' . time() . '.json', json_encode($payload, JSON_PRETTY_PRINT));
             Log::info("Message Incoming from AHA");
             Log::info($payload);
-            
+
             $this->AHAPayload->setPayload($payload);
 
             $message = $this->AHAPayload->makeSlackMessage();
